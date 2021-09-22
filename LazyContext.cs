e74d26a16cdee53785582ea41e808134547f838a
CochachyLuis <luis.cochachi@vaetech.net>
2021-09-21 19:29:35 -0500

    Agregar archivos de proyecto.

---
+++ LazyContext.cs
@@ -0,0 +1,15 @@
+﻿namespace System.Data.LazyContext
+{
+    public abstract class LazyContext<TContext> where TContext : class
+    {
+        readonly Lazy<TContext> __context;
+        readonly Func<TContext> __contextFactory;
+        public LazyContext(Func<TContext> contextFactory)
+        {
+            __contextFactory = contextFactory;
+            __context = new Lazy<TContext>(contextFactory);
+        }
+        public TContext GetContext() => __context.Value;
+        public TContext NewContext() => new Lazy<TContext>(__contextFactory).Value;
+    }
+}
